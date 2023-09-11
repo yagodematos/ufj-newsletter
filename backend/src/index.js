d0bd60b6d6dca3a351c90466ef568795f10c12be
Yago de Matos <contato.yagodematos@gmail.com>
2023-09-10 21:53:48 -0300

    finish backend

---
+++ backend/src/index.js
@@ -0,0 +1,67 @@
+const fastify = require('fastify');
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'admin',
+    password: 'senha',
+    database: 'newsletter'
+});
+
+const app = fastify();
+
+
+app.get('/users', (request, reply) => {
+    connection.query(
+        'SELECT * FROM `users`',
+        (err, results) => {
+            if (err) {
+                console.log('Connection error: ' + err);
+                process.exit()
+            }
+
+            if (results.length <= 0) {
+                reply.status(200).send([]);
+            }
+
+            reply.status(200).send(results);
+        }
+    )
+});
+
+app.post('/users', (request, reply) => {
+    const {name, email} = request.body;
+
+    connection.query(
+        'INSERT INTO users(name, email) VALUES (?, ?);',
+        [name, email],
+        (err, result) => {
+            if (err) {
+                reply.send({ error: err });
+            }
+
+            reply.send(result)
+        });
+});
+
+app.post('/newsletter', (request, reply) => {
+    const {message} = request.body;
+
+    connection.query(
+        'SELECT name, email FROM `users`',
+        (err, result) => {
+            if (err) {
+                console.log('Newsletter error: ' + err);
+                process.exit()
+            }
+
+            reply.send({
+                "users": result,
+                message
+            })
+        }
+    )
+});
+
+
+app.listen({ port: 3000 })
